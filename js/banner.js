adcee7c8146c42d093848337c0ade9b0f4f73735
unknown <Brandon@DESKTOP-6QU7QI6.BARSANDPERU.LOCAL>
2022-06-22 15:16:29 -0500

    banner definido

---
+++ js/banner.js
@@ -0,0 +1,75 @@
+window.addEventListener('load', function(){
+    let  slider = new Glider(document.querySelector('.glider__banner'), {
+         slidesToShow:1,
+         slidesToScroll: 1,
+         draggable: true,
+         dots: '.dots',
+         arrows: {
+             prev: '.glider-prev',
+             next: '.glider-next'
+         },
+         responsive: [
+             {
+               // screens greater than >= 775px
+               breakpoint: 800,
+               settings: {
+                 // Set to `auto` and provide item width to adjust to viewport
+                 slidesToShow: 'auto',
+                 slidesToScroll: 'auto',
+                 
+               }
+             },{
+               // screens greater than >= 1024px
+               breakpoint: 1024,
+               settings: {
+                 slidesToShow: 1,
+                 slidesToScroll: 1,
+                 itemWidth: 150,
+                 duration: 0.25
+               }
+             }
+           ]
+     })
+     
+     
+     imprimirYear(yaerActual)
+     slideAutoPaly(slider, '.glider__banner');
+ 
+     function slideAutoPaly(glider, selector, delay = 3000, repeat = true) {
+     let autoplay = null;
+     const slidesCount = glider.track.childElementCount;
+     let nextIndex = 1;
+     let pause = true;
+ 
+     function slide() {
+         autoplay = setInterval(() => {
+             if (nextIndex >= slidesCount) {
+                 if (!repeat) {
+                     clearInterval(autoplay);
+                 } else {
+                     nextIndex = 0;
+                 }
+             }
+             glider.scrollItem(nextIndex++);
+         }, delay);
+     }
+ 
+     slide();
+ 
+     var element = document.querySelector(selector);
+     element.addEventListener('mouseover', (event) => {
+         if (pause) {
+             clearInterval(autoplay);
+             pause = false;
+         }
+     }, 300);
+ 
+     element.addEventListener('mouseout', (event) => {
+         if (!pause) {
+             slide();
+             pause = true;
+         }
+     }, 300);
+ }
+  
+   })
