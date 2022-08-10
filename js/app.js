c3ac01d62cc2a1c04191f42f468f9ca1e0a1da96
unknown <Brandon@DESKTOP-6QU7QI6.BARSANDPERU.LOCAL>
2022-08-10 18:15:54 -0500

    cambios

--- js/app.js
+++ js/app.js
@@ -1,84 +1,12 @@
-window.addEventListener('load', function(){
-   let  slider = new Glider(document.querySelector('.glider'), {
-        slidesToShow:1,
-        slidesToScroll: 1,
-        draggable: true,
-        dots: '.dots',
-        arrows: {
-            prev: '.glider-prev',
-            next: '.glider-next'
-        },
-        responsive: [
-            {
-              // screens greater than >= 775px
-              breakpoint: 800,
-              settings: {
-                // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: 'auto',
-                slidesToScroll: 'auto',
-                
-              }
-            },{
-              // screens greater than >= 1024px
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                itemWidth: 150,
-                duration: 0.25
-              }
-            }
-          ]
-    })
-    
-    
-    imprimirYear(yaerActual)
-    slideAutoPaly(slider, '.glider');
+const btnUno = document.querySelector("#buton-1")
+const cardUno = document.querySelector("#card-1")
+const cardimagenUno = document.querySelector("#imagen-card-1")
 
-    function slideAutoPaly(glider, selector, delay = 3000, repeat = true) {
-    let autoplay = null;
-    const slidesCount = glider.track.childElementCount;
-    let nextIndex = 1;
-    let pause = true;
-
-    function slide() {
-        autoplay = setInterval(() => {
-            if (nextIndex >= slidesCount) {
-                if (!repeat) {
-                    clearInterval(autoplay);
-                } else {
-                    nextIndex = 0;
-                }
-            }
-            glider.scrollItem(nextIndex++);
-        }, delay);
-    }
-
-    slide();
-
-    var element = document.querySelector(selector);
-    element.addEventListener('mouseover', (event) => {
-        if (pause) {
-            clearInterval(autoplay);
-            pause = false;
-        }
-    }, 300);
-
-    element.addEventListener('mouseout', (event) => {
-        if (!pause) {
-            slide();
-            pause = true;
-        }
-    }, 300);
+document.addEventListener('DOMContentLoaded',()=>{
+   btnUno.addEventListener('click',clicked)
+})
+  
+function clicked(){
+    cardUno.removeChild(btnUno)
+    cardimagenUno.src("card-cambio.png")
 }
- 
-  })
-  
- // colocar año actual
-  const year = new Date()
-  const  yaerActual = year.getFullYear()
-  const imprimirYear = (leatra) =>{
-     return document.querySelector("#year").innerHTML = leatra
-  }
-  
-
