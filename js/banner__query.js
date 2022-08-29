453fdba1d3152758dd46429bf8efba4e059a33c1
unknown <Brandon@DESKTOP-6QU7QI6.BARSANDPERU.LOCAL>
2022-08-29 12:00:50 -0500

    logo

--- js/banner__query.js
+++ js/banner__query.js
@@ -18,13 +18,18 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 function loader(){
   const div = document.createElement("div")
-  div.classList.add("loader__","position-absolute","d-flex","justify-content-center","align-items-center","position-fixed")
+  div.classList.add("loader__","position-absolute","d-flex","justify-content-center","align-items-center","position-fixed","flex-column")
   const spinner = document.createElement("div")
   spinner.classList.add("spinner")
+  const img = document.createElement("img")
+  img.src="/img/logodiutin.png"
+
+  img.style.width='200px'
   const spinnerUno = document.createElement("div")
   spinnerUno.classList.add("double-bounce1")
   const spinnerDos = document.createElement("div")
   spinnerDos.classList.add("double-bounce2")
+  div.appendChild(img)
   spinner.appendChild(spinnerUno)
   spinner.appendChild(spinnerDos)
   div.appendChild(spinner)
